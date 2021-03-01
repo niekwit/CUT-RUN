24e62110427dd4a0c6c345e19b734e683f909c50
niekwit <41302859+niekwit@users.noreply.github.com>
2021-03-01 16:50:27 +0000

    updated

--- cutrun.sh
+++ cutrun.sh
@@ -6,8 +6,10 @@ threads=""
 rename_config=""
 genome=""
 dedup=""
-cutoff=""
+cutoff=0
 align_mm=0
+big_wig=""
+reads_length=""
 
 PICARD=$(find $HOME -name picard.jar)
 SCRIPT_DIR=$(find $HOME -type d -name "CUT-RUN")
@@ -17,7 +19,7 @@ function usage {
 	exit 2
 }
 
-while getopts 't:g:rdm:f:?h' c
+while getopts 't:g:rdm:f:bl?h' c
 do
 	case $c in
 		t) 
@@ -32,6 +34,10 @@ do
 			align_mm=$OPTARG;;
         	f) 
 			cutoff=$OPTARG;;
+		b) 	
+			big_wig="TRUE";;
+		l)
+			reads_length="TRUE";;	
 		h|?) 	
 			usage;;
 	esac
@@ -53,7 +59,7 @@ if [[ ! $cutoff =~ ^-?[0-9]+$ ]];
 		exit 1
 fi
 
-if [[ -z "$threads" ]] && [[ $threads =~ ^-?[0-9]+$ ]];
+if [[ -z "$threads" ]] 
 	then
 		threads=$(nproc --all)
 elif [[ ! -z "$threads" ]] && [[ ! $threads =~ ^-?[0-9]+$ ]]
@@ -113,14 +119,15 @@ else
 	echo "Trimming already performed"	
 fi
 
-#aligning samples (bowtie2 settings:Skene et al 2018 Nature Protocols)
+#aligning samples 
 bam_folder="bam/"
+
 if [[ ! -d  "$bam_folder" ]];
 	then
 		mkdir -p bam
-		touch align.log
-		index_path=$(cat "$SCRIPT_DIR/config.yml" | shyaml get-value genome.$genome.0 | awk '{print$2}')
-		#black_list_path=$(cat "$SCRIPT_DIR/config.yml" | shyaml get-value genome.$genome.1 | awk '{print$2}')
+		#touch align.log
+		index_path=$(cat "$SCRIPT_DIR/genome.yml" | shyaml get-value genome.$genome.0 | awk '{print$2}')
+		#black_list_path=$(cat "$SCRIPT_DIR/genome.yml" | shyaml get-value genome.$genome.1 | awk '{print$2}')
 		for read1 in trim/*"_1_val_1.fq.gz"
 		do 
 			read2="${read1%_1_val_1.fq.gz}_2_val_2.fq.gz"
@@ -130,10 +137,14 @@ if [[ ! -d  "$bam_folder" ]];
 			align_output="bam/$base_name.bam"
 			echo "Aligning $base_name"
 			bowtie2 -p "$threads" --local --very-sensitive-local --no-unal --no-mixed --no-discordant --phred33 -I 10 -X 700 -x "$index_path" -1 "$read1" -2 "$read2" 2>> align.log | samtools view -q 15 -F 260 -bS -@ "$threads" - | samtools sort -@ "$threads" - > "$align_output"
+			samtools index -b -@$threads "$align_output"
 		done
 else
 	echo "Alignment already performed"
 fi                                                                                                                                      
+
+#--dovetail --phred33
+#--local --very-sensitive-local --no-unal --no-mixed --no-discordant --phred33 -I 10 -X 700
 
 #deduplication
 dedup_folder="deduplication/"
@@ -152,6 +163,127 @@ else
     echo "Deduplication already performed/not selected"
 fi
 
+#generate histogram of reads lengths
+#GSE125988
+
+echo "Generating histograms of read counts"
+reads_length_folder=read-counts
+if [[ "$reads_length" == "TRUE" ]] && [[ ! -d  "$reads_length_folder" ]];
+	then
+		mkdir "$reads_length_folder"
+		for file in bam/*.bam
+		do 
+			base_file="${file%.bam}"
+			base_file="${base_file##*/}"
+			out_file="$reads_length_folder/read-length-$base_file.txt"
+			echo "count read-length" > "$reads_length_folder"/read-length-$base_file.txt
+			samtools view -@$threads "$file" | head -n 1000000 | cut -f 10 | perl -ne 'chomp;print length($_) . "\n"' | sort | uniq -c >> "$out_file"
+		done
+	else
+		echo "Read count histograms already generated"
+fi
+
+#echo "count read-length" > read-length.txt
+#samtools view -@48 bam/CUTRUN.H3K4me3.bam | head -n 1000000 | cut -f 10 | perl -ne 'chomp;print length($_) . "\n"' | sort | uniq -c >> read-length.txt
+
+: <<'END'
+#create bigWig files
+#load bamCoverage settings:
+binsize=$(cat settings.yaml | shyaml get-value BigWig.binSize)
+normalizeusing=$(cat settings.yaml | shyaml get-value BigWig.normalizeUsing)
+extendreads=$(cat settings.yaml | shyaml get-value BigWig.extendReads)
+effectivegenomesize=$(cat settings.yaml | shyaml get-value BigWig.effectiveGenomeSize)
+#checking bamCoverage settings:
+if [[ ! $binsize =~ ^-?[0-9]+$ ]];
+	then
+		echo "ERROR: binSize should be an integer."
+		exit 1
+fi
+
+if [[ $normalizeusing != "RPKM" ]] && [[ $normalizeusing != "CPM" ]] && [[ $normalizeusing != "BPM" ]] && [[ $normalizeusing != "RPGC" ]] && [[ $normalizeusing != "None" ]];
+	then
+		echo "ERROR: invalid normalisation method chosen."
+		echo "Available methods: RPKM, CPM, BPM, RPGC or None"
+		exit 1
+fi
+
+if [[ ! $extendreads =~ ^-?[0-9]+$ ]];
+	then
+		echo "ERROR: extendReads should be an integer."
+		exit 1
+fi
+
+if [[ ! $effectivegenomesize =~ ^-?[0-9]+$ ]];
+	then
+		echo "ERROR: effectiveGenomeSize should be an integer."
+		echo "See https://deeptools.readthedocs.io/en/latest/content/feature/effectiveGenomeSize.html"
+		exit 1
+fi
+
+
+#creates bigWig files
+bigwig_dir=bigwig_bin$binsize
+mkdir -p "$bigwig_dir"
+
+function big_wig {
+	bigwig_output="${file%-dedupl-sort-bl.bam}-norm.bw"
+	bigwig_output=${bigwig_output##*/}
+	bamCoverage -p $max_threads --binSize "$binsize" --normalizeUsing "$normalizeusing" --extendReads "$extendreads" --effectiveGenomeSize "$effectivegenomesize" -b $file -o $bigwig_dir/$bigwig_output 2>> bigwig.log
+}  
+
+
+echo "Creating BigWig files"
+dedup_folder="deduplication/"
+
+if [[ ! -d  "$dedup_folder" ]];
+	then
+		
+
+
+
+
+
+sorted_bam=$(ls -l bam/*dedupl-sort-bl.bam 2> /dev/null | wc -l) #returns zero without *dedupl-sort-bl.bam files
+if [[ $sorted_bam != 0 ]]; 
+then
+	index_bam=$(ls -l bam/*dedupl-sort-bl.bam.bai 2> /dev/null | wc -l)
+	if [[ $index_bam == 0 ]];
+	then
+		for file in bam/*dedupl-sort-bl.bam
+		do 
+			samtools index -@ $max_threads -b $file 2>> bigwig.log
+			big_wig
+		done
+	elif [[ $index_bam != 0 ]];
+	then
+		for file in bam/*dedupl-sort-bl.bam
+		do 
+			bigwig_output="${file%-dedupl-sort-bl.bam}-norm.bw"
+			big_wig
+		done
+	fi
+elif [[ $sorted_bam == 0 ]]; 
+then	
+	index_bam=$(ls -l bam/*sort-bl.bam.bai 2> /dev/null | wc -l)
+	if [[ $index_bam == 0 ]];
+	then
+		for file in bam/*sort-bl.bam
+		do 
+			samtools index -@ $max_threads -b $file 2>> bigwig.log
+			big_wig
+		done
+	elif [[ $index_bam != 0 ]];
+	then
+		for file in bam/*sort-bl.bam
+		do 
+			big_wig
+		done
+	fi		
+fi
+	
+END
+
+
 #filter out reads below cutoff
 filter_folder="filter/"
 
@@ -161,24 +293,29 @@ function filter_reads {
 	samtools view -h -@ "$threads" "$bam" | awk -v x=$cutoff 'length($10) < x || $1 ~ /^@/' | samtools view -bS -@ "$threads" - > "filter/$base_name.${cutoff}bp.bam"
 } #function to filter reads out below given cutoff
 
-if [[ ! -d  "$filter_folder" ]] && [[ -d  "$dedup_folder" ]];
+if [[ "$cutoff" == 0 ]];
 	then
-		mkdir filter
-		echo "Filtering reads <${cutoff} bp"
-		for bam in deduplication/*.bam
-		do
-			filter_reads
-		done
-elif [[ ! -d  "$filter_folder" ]] && [[ ! -d  "$dedup_folder" ]];
-	then
-		mkdir filter
-		echo "Filtering reads <${cutff} bp"
-		for bam in bam/*.bam
-		do
-			filter_reads
-		done
-else
-	echo "Size filtering already performed"
+		echo "No size filtering of reads selected"
+	else
+		if [[ ! -d  "$filter_folder" ]] && [[ -d  "$dedup_folder" ]];
+			then
+				mkdir filter
+				echo "Filtering reads <${cutoff} bp"
+				for bam in deduplication/*.bam
+				do
+					filter_reads
+				done
+		elif [[ ! -d  "$filter_folder" ]] && [[ ! -d  "$dedup_folder" ]];
+			then
+				mkdir filter
+				echo "Filtering reads <${cutff} bp"
+				for bam in bam/*.bam
+				do
+					filter_reads
+				done
+		else
+			echo "Size filtering already performed"
+		fi
 fi
 
 #peak calling
